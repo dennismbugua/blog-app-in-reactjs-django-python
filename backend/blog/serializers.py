46f4fd2bf10666962e5c813f2508ca690275c66b
dennismbugua <dennis@starlaunch.org>
2023-06-06 13:29:40 +0300

    what you did

--- backend/blog/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import BlogPost
-
-class BlogPostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BlogPost
-        fields = '__all__'
-        lookup_field = 'slug'
--- backend/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import BlogPostListView, BlogPostDetailView, BlogPostFeaturedView, BlogPostCategoryView
-
-urlpatterns = [
-    path('', BlogPostListView.as_view()),
-    path('featured', BlogPostFeaturedView.as_view()),
-    path('category', BlogPostCategoryView.as_view()),
-    path('<slug>', BlogPostDetailView.as_view()),
-]
